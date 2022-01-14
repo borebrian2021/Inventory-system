b1636d33f440e70161a91bb3fb8c09af03107e9d
borebrian2021 <bkimutai2021@gmail.com>
2022-01-14 02:07:02 -0800

    Add project files.

---
+++ SAINA/DBContext/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SAINA.Models;
+
+namespace Lubes.DBContext
+{
+	public class ApplicationDBContext : DbContext
+	{
+        public DbSet<Clients> Clients { get; set; }
+        public DbSet<Deliveries> Deliveries { get; set; }
+        public DbSet<Invoices> Invoices { get; set; }
+        public DbSet<Particulars> Particulars { get; set; }
+        public DbSet<Receipts> Receipts { get; set; }
+        public DbSet<Log_in> Log_in { get; set; }
+
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+			: base(options)
+		{
+		}
+	}
+}
